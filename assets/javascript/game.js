cc452439b0d733c918cdf6cb52ea0de89f184756
jivinjules <juliehodges05@gmail.com>
2018-07-12 14:06:53 -0500

    final edit, fully functional

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,77 +1,84 @@
 //Variables
-//Letters to be guessed
 
-//words for computer to randomly display
 var beachWords = ["ocean", "sand", "palmtree", "coconut", "towel", "surfboard", "hawaii", "seashell", "sandcastle", "sailboat"];
+var chosenWord = "";
+var lettersInChosenWord = [];
+var numberOfBlanks = 0;
+var blanksAndUnderscores = [];
+var wrongGuesses = [];
+var letterGuessed = "";
+var winCount = 0;
+var lossCount = 0;
+var numberOfGuesses = 10;
 
-for (var i = 0; i < beachWords.length; i++) {
-        console.log(beachWords[i]);
-}
-console.log("---------");
-
-var startGame = false; //starts game
-var endGame = false; //turn to true to end game
-var guesses = 12; //# of guesses  (max 12)
-var lettersGuessed = []; //holds the letters guessed
-var winCount = 0; //keeps track of number of wins
-var randomWord = beachWords[Math.floor(Math.random() * beachWords.length)];
-var underscore = [];
-var winMessage = ("Winner! Winner! Chicken Dinner!");
-var loseMessage = ("You lose! Better luck next time!")
-
-document.onkeyup = function (event) {
-        var userGuess = event.key;
-        console.log(event.key);
-        lettersGuessed.push(userGuess); //Used to determine what user has selected
-        startGame = true;
-        guesses--;
-        document.getElementById("guessesleft").innerHTML = guesses;
-        document.getElementById("lettersguessed").innerHTML = lettersGuessed;
-        console.log("This is my random word " + randomWord);
-
-        for (var i = 0; i < randomWord.length; i++) {
-                underscore[i] = "_  ";
-                // console.log(randomWord);
+// When game is started, the computer chooses a word from the beachwords list
+//The word is split apart
+//Each letter is replaced by a blank
+//The word is looped through and replaced with an underscore
+function startGame() {
+        numberOfGuesses = 10;
+        chosenWord = beachWords[Math.floor(Math.random() * beachWords.length)];
+        lettersInChosenWord = chosenWord.split("");
+        numberOfBlanks = lettersInChosenWord.length;
+        blanksAndUnderscores = [];
+        wrongGuesses = [];
+        for (var i = 0; i < numberOfBlanks; i++) {
+                blanksAndUnderscores.push("_ ");
         }
+        //wrong guesses are added to the index
+        document.getElementById("lettersguessed").innerHTML = wrongGuesses.join(" ");
+        //number of guesses is added to the html
+        document.getElementById("guessesleft").innerHTML = numberOfGuesses;
+        //word as underscore is updated to html
+        document.getElementById("words").innerHTML = blanksAndUnderscores.join(" ");
+}
 
-        //user guesses a letter
-        // Rudolph.indexOf(t) == -1 This help for me
-
-        if (randomWord.indexOf(userGuess) == -1) {
-                //decrement guesses
-
-                //  guesses--;
-                document.getElementById("guessesleft").innerHTML = guesses;
-
-
-        } else {
-                for (let i = 0; i < randomWord.length; i++) {
-                        console.log(randomWord[i]);
-                        if (userGuess === randomWord[i]) {
-                                //    console.log("Troubleshooting my if statement")
-
-                                underscore[i] = userGuess;
-                                console.log(underscore[i]);
-                                console.log(underscore);
-
-                                document.getElementById("words").innerHTML = underscore.join("");
-                                // console.log(randomWord.charAt() + userGuess);
-
-                        } //for loop ends
-                        //        console.log(randomWord + userGuess + guesses);
-
+//function to check whether the letter guessed is in the word or not
+//If letter is in word, it is pushed into the blanks and underscores
+//If letter is not in word, it is pushed to wrong Guesses
+//The number of guesses decrements by one
+function checksLetters(letter) {
+        var letterInWord = false;
+        for (var i = 0; i < numberOfBlanks; i++) {
+                if (chosenWord[i] === letter) {
+                        letterInWord = true;
                 }
-
+        }
+        if (letterInWord) {
+                for (var j = 0; j < numberOfBlanks; j++) {
+                        if (chosenWord[j] === letter) {
+                                blanksAndUnderscores[j] = letter;
+                        }
+                }
+        } else {
+                wrongGuesses.push(letter);
+                numberOfGuesses--;
         }
 }
-//If user wins, win count goes up by one, and the win Message comes on screen
-if (randomWord === underscore) {
-        winCount++;
-        alert(winMessage);
+//All html is updated
+function roundComplete() {
+        document.getElementById("guessesleft").innerHTML = numberOfGuesses;
+        document.getElementById("words").innerHTML = blanksAndUnderscores.join(" ");
+        document.getElementById("lettersguessed").innerHTML = wrongGuesses.join(" ");
+
+        if (lettersInChosenWord.toString() === blanksAndUnderscores.toString()) {
+                winCount++;
+                alert("Winner! Winner! Chicken Dinner!");
+                document.getElementById("wins").innerHTML = winCount;
+                startGame();
+        }
+        else if (numberOfGuesses === 0) {
+                lossCount++;
+                alert("You Lose! Better Luck Next Time!");
+                document.getElementById("losses").innerHTML = lossCount;
+                startGame();
+        }
+
 }
 
-//If guesses gets to zero, the lose message appears on screen
-if (guesses === 0) {
-        alert(loseMessage);
-}
-
+startGame();
+document.onkeyup = function(event) {
+        letterGuessed = String.fromCharCode(event.which).toLowerCase();
+        checksLetters(letterGuessed);
+        roundComplete();
+};
